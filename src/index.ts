d8950648576ef1ebd164fc3bcdc85ce1343a5502
ridarf <rida.fkih@jumbleberry.com>
2021-09-24 21:35:42 -0600

    include response in log

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,6 @@ import { Relay } from "@modules/relay.module";
 
 const relay = new Relay(24);
 
-relay.timedToggle(2000).then(() => console.log("complete"));
+relay
+  .timedToggle(2000)
+  .then((response) => console.log("complete: " + response));
