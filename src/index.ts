17e5d3c71f517ded704487359a3a6a064118306e
ridarf <Getridahim@gmail.com>
2021-09-25 06:26:17 -0600

    use express server

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
+import * as express from "express";
 import { Relay } from "@modules/relay.module";
 
+const app = express();
 const relay = new Relay(24);
 
-(async () => {
-  await relay.timedToggle(2000);
-  relay.dispose();
-})();
+app.post("/trigger/garage", async (req, res) => {
+  const success = await relay.timedToggle(500);
+  if (!success) return res.status(500).json({ success: false });
+  res.status(200).json({ success: true });
+});
+
+app.listen(4321, () => ":: server listening");
