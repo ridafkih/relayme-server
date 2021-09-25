16d702039a4843f1bdba731fd9fe1fc53e9f4b3e
ridarf <rida.fkih@jumbleberry.com>
2021-09-24 21:39:32 -0600

    just deactivate

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,5 @@ import { Relay } from "@modules/relay.module";
 
 const relay = new Relay(24);
 
-relay
-  .timedToggle(2000)
-  .then((response) => console.log("complete: " + response));
+relay.deactivate();
+console.log("done");
