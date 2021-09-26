fe72c17020b74072d284ef6cd1eb0b9f65c6547f
ridarf <Getridahim@gmail.com>
2021-09-26 17:32:10 -0600

    remove relay

--- src/routes/paths/generate-token.ts
+++ src/routes/paths/generate-token.ts
@@ -4,16 +4,15 @@ import App from "@modules/app";
 import Endpoint from "@typings/Route";
 import { getFileFromPath } from "@helpers/filesystem";
 import { success } from "@helpers/response";
-import { triggerGarageDoor } from "@services/relay";
+import { generateDeviceIdentifier } from "@services/identity";
 
 const fileName = getFileFromPath(__filename, __dirname);
 
-export const toggleGarage: Endpoint = {
-  name: "post:toggle-garage",
+export const generateToken: Endpoint = {
+  name: "post:generate-token",
   method: "post",
   path: `/${fileName}`,
   callback: async function (this: App, req: Request, res: Response) {
-    const successful = await triggerGarageDoor();
-    success(res, { successful });
+    
   },
 };
--- src/services/relay.ts
+++
@@ -1,5 +0,0 @@
-import { Relay } from "@modules/relay";
-
-export const garageDoor = new Relay(24);
-
-export const triggerGarageDoor = () => garageDoor.timedToggle(500);
