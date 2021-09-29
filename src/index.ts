7e8df572ae1a70ad9a8aff56c2cf92b157141587
ridarf <rida.fkih@jumbleberry.com>
2021-09-29 00:44:33 -0600

    clean up index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
-import { Client } from "pg";
-
 import App from "@modules/app";
-import Database from "@modules/database";
 
 import Route from "@typings/Route";
 
@@ -9,22 +6,7 @@ import * as routes from "@routes/index";
 
 const endpointObjects: Route[] = Object.values(routes);
 
-const {
-  DATABASE_URL,
-  PGUSER = "defaultUser",
-  PGPASSWORD = "defaultPassword",
-  PGDATABASE = "defaultDatabase",
-} = process.env;
-
-const databaseClient = new Client({
-  connectionString: DATABASE_URL,
-  user: PGUSER,
-  password: PGPASSWORD,
-  database: PGDATABASE,
-});
-
-const database = new Database(databaseClient);
-const app = new App(database);
+const app = new App();
 
 app.registerEndpoints(endpointObjects);
 app.initialize();
