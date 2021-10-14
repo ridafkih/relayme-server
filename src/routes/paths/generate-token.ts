669ffa6026fda6ad2e3c0007b335eae9e2f006b7
Rida F'kih <hello@rida.dev>
2021-10-13 18:44:46 -0600

    reject device registration if user is not authed

--- src/routes/paths/generate-token.ts
+++ src/routes/paths/generate-token.ts
@@ -5,12 +5,14 @@ import Endpoint from "@typings/Route";
 import { getFileFromPath } from "@helpers/filesystem";
 import { registerNewDevice } from "@helpers/database";
 import { success } from "@helpers/response";
+import { rejectNoAuth } from "@middleware/authentication";
 
 const fileName = getFileFromPath(__filename, __dirname);
 
 export const generateToken: Endpoint = {
   name: "post:generate-token",
   method: "post",
+  middleware: [rejectNoAuth],
   path: `/${fileName}`,
   callback: async function (this: App, req: Request, res: Response) {
     const data = await registerNewDevice();
